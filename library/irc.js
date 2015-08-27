843d68f2a1be86f9e2b63cbc6cde290f5be36015
Anthony Van de Gejuchte <anthonyvdgent@yahoo.com>
2015-08-27 23:21:31 +0200

    Make the bot alive

--- library/irc.js
+++ library/irc.js
@@ -5,8 +5,20 @@ var ircBot = function(settings) {
 };
 
 ircBot.prototype.connect = function() {
-    this.client = new irc.Client('https://chat.freenode.net', 'Alice-AI', {
+    this.client = new irc.Client('chat.freenode.net', 'Alica', {
         channels: ['#screeps'],
+    });
+
+    this.client.addListener('message', function (from, to, message) {
+        console.log(from + ' => ' + to + ': ' + message);
+    });
+
+    this.client.addListener('pm', function (from, message) {
+        console.log(from + ' => ME: ' + message);
+    });
+
+    this.client.addListener('error', function(message) {
+        console.log('error: ', message);
     });
 };
 
@@ -17,4 +29,4 @@ ircBot.prototype.disconnect = function(reason) {
 
 module.exports = {
     ircBot: ircBot
-}
+};
